c499d3fd0fe5ed91f727efec38fb467d1a67ac5f
alenfeng <alenfeng@tencent.com>
2023-03-23 18:29:14 +0800

    修改base

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
-    name: 'npm',
+    name: 'xiaocheng',
   },
+  base: '/Hell-World'
 });
